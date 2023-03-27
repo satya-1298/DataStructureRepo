91a5921bda5eb824b2e1c5b479cfa8c21141d3bb
unknown <satya-1298>
2023-03-27 16:32:43 +0530

    Pushing Elements

---
+++ DataStructure/Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public  class Stack
+    {
+        public static void Display()
+        {
+            Stack<int> s = new Stack<int>();
+            s.Push(70);
+            s.Push(30);
+            s.Push(56);
+            Console.WriteLine("Elements in stack is:");
+            foreach (int i in s) 
+            {
+               // Console.WriteLine("Elements in stack is:");
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

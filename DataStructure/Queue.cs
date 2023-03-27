be37e06042a741ec916bb93186710f5fec22c180
unknown <satya-1298>
2023-03-27 17:05:10 +0530

    Entering Element

---
+++ DataStructure/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public  class Queue
+    {
+        public static void Display()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            foreach (var item in queue) 
+            {
+            Console.WriteLine(item);
+            }
+            int q=queue.Count();
+            Console.WriteLine("Count is "+q);
+           
+        }
+    }
+}
